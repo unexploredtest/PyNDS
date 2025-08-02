a95765718524e294f6dddbb954977e531af05148
unexploredtest <unexploredtest@tutanota.com>
2025-08-03 02:12:57 +0330

    cNDS: Separate some nds methods definitions

--- cnds/cnds.cpp
+++ cnds/cnds.cpp
@@ -17,6 +17,8 @@ NB_MODULE(cnds, m) {
         .def("get_gba_frame", &Nds::getGbaFrame)
         .def("get_top_nds_frame", &Nds::getTopNdsFrame)
         .def("get_bot_nds_frame", &Nds::getBotNdsFrame)
+
+        // Input methods
         .def("set_touch_input", &Nds::setTouchInput)
         .def("clear_touch_input", &Nds::clearTouchInput)
         .def("save_state", &Nds::saveState)
@@ -24,8 +26,33 @@ NB_MODULE(cnds, m) {
         .def("touch_input", &Nds::touchInput)
         .def("release_touch_input", &Nds::releaseTouchInput)
         .def("press_key", &Nds::pressKey)
-        .def("release_key", &Nds::releaseKey);
+        .def("release_key", &Nds::releaseKey)
 
+        // Read memory methods
+        .def("read_ramu8", &Nds::readRamu8)
+        .def("read_ramu16", &Nds::readRamu16)
+        .def("read_ramu32", &Nds::readRamu32)
+        .def("read_ramu64", &Nds::readRamu64)
+        .def("read_rami8", &Nds::readRami8)
+        .def("read_rami16", &Nds::readRami16)
+        .def("read_rami32", &Nds::readRami32)
+        .def("read_rami64", &Nds::readRami64)
+        .def("read_ramf32", &Nds::readRamf32)
+        .def("read_ramf64", &Nds::readRamf64)
+        .def("read_map", &Nds::readMap)
+
+        // Write memory methods
+        .def("write_ramu8", &Nds::writeRamu8)
+        .def("write_ramu16", &Nds::writeRamu16)
+        .def("write_ramu32", &Nds::writeRamu32)
+        .def("write_ramu64", &Nds::writeRamu64)
+        .def("write_rami8", &Nds::writeRami8)
+        .def("write_rami16", &Nds::writeRami16)
+        .def("write_rami32", &Nds::writeRami32)
+        .def("write_rami64", &Nds::writeRami64)
+        .def("write_ramf32", &Nds::writeRamf32)
+        .def("write_ramf64", &Nds::writeRamf64)
+        .def("write_map", &Nds::writeMap);
 
     m.def("set_direct_boot", &Config::setDirectBoot, "Set direct boot mode");
     m.def("set_frame_skip", &Config::setFrameSkip, "Set frame skip value");
--- cnds/nds.cpp
+++ cnds/nds.cpp
@@ -82,27 +82,3 @@ void Nds::loadState(std::string path) {
     m_core->saveStates.loadState();
 }
 
-void Nds::setTouchInput(int x, int y) {
-    m_core->spi.setTouch(x, y);
-}
-
-void Nds::clearTouchInput() {
-    m_core->spi.clearTouch();
-}
-
-void Nds::touchInput() {
-    m_core->input.pressScreen();
-}
-
-void Nds::releaseTouchInput() {
-    m_core->input.releaseScreen();
-}
-
-void Nds::pressKey(int key) {
-    m_core->input.pressKey(key);
-}
-
-void Nds::releaseKey(int key) {
-    m_core->input.releaseKey(key);
-}
-
