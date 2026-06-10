9ef4e196c84709b510b0ab27e5ba0c3c9ba5d9ff
unexploredtest <unexploredtest@tutanota.com>
2026-06-10 22:44:15 +0330

    Remove unnecessary files and change methods ordering in cnds

--- main.py
+++
@@ -1,9 +0,0 @@
-import pynds,time
-rom_path = "rom/SoulSilver.nds"  # path to your .nds file
-nds = pynds.PyNDS(rom_path)
-
-nds.open_window(width=512, height=500)  # opens pygame window automatically
-while nds.window.running:
-    nds.tick()      # runs emulator until next frame
-    nds.render()    # renders both screens using built-in window.py
-    #time.sleep(0.005)   #time stop 
